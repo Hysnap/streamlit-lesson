dfd5211155b7cc12a2978898da088641c0ccf41c
PaulGolder-DBC <pgolder1972@gmail.com>
2025-02-14 06:14:06 +0000

    Updated Streamlit test app

--- 2_app_with_multiple_pages.py
+++ 2_app_with_multiple_pages.py
@@ -1,1 +1,18 @@
+import streamlit as st
+from app_pages.multi_page import MultiPage
 
+# load pages scripts
+from app_pages.page1 import page1_body
+from app_pages.page2 import page2_body
+from app_pages.page3 import page3_body
+from app_pages.page_calculator import calculator_body
+
+app = MultiPage(app_name= "This is my first App at Streamlit!!!")  # Create an instance
+
+# Add your app pages here using .add_page()
+app.add_page("Page 1", page1_body)
+app.add_page("Page 2", page2_body)
+app.add_page("Page 3", page3_body)
+app.add_page("Calculator", calculator_body)
+
+app.run() # Run the  app
